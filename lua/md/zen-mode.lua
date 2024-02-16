ea197d7652481a3952711fce8c76fa2ee5af8ae4
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-02-16 21:09:53 +0700

    Zenmode tweaking

--- lua/md/zen-mode.lua
+++ lua/md/zen-mode.lua
@@ -1,12 +1,44 @@
-local ZEN = {
-  'folke/zen-mode.nvim',
+return {
+  {
+    'folke/zen-mode.nvim',
+    dependencies = 'folke/twilight.nvim',
+    keys = { "<leader>z", "<cmd>ZenMode<CR>" },
+    cmd = "ZenMode",
+    config = function()
+      local status_ok, zen = pcall(require, 'zen-mode')
+      if not status_ok then
+        return
+      end
+
+      zen.setup({
+        window = {
+          options = {
+            number = false,
+            relativenumber = false,
+            cursorline = false,
+            cursorcolumn = false,
+            foldcolumn = "0",
+            list = false,
+          }
+        },
+        plugins = {
+          options = { enabled = true, ruler = false, showcmd = false },
+          twilight = { enabled = true },
+          gitsigns = { enabled = false },
+          tmux = { enabled = false }
+        }
+      })
+      vim.keymap.set('n', "<leader>z", "<cmd>ZenMode<CR>", {})
+    end
+  },
+  {
+    'folke/twilight.nvim',
+    config = function()
+      local status_ok, twilight = pcall(require, 'twilight')
+      if not status_ok then
+        return
+      end
+      twilight.setup({})
+    end
+  }
 }
-
-function ZEN.config()
-  local status_ok, zen = pcall(require, 'zen-mode')
-  if not status_ok then
-    return
-  end
-
-  zen.setup({})
-end
