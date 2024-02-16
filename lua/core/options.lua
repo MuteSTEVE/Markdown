65bf078efcaba5eabcb9cfb456de62d13b1f9fc2
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-02-16 14:41:28 +0700

    Using neovim zellner colorscheme to start writing

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,46 +1,46 @@
 local options = {
-	backup = false,
-	cmdheight = 1,
-	completeopt = { "menuone", "noselect" },
-	conceallevel = 1,
-	cursorline = true,
-	expandtab = true,
-	fileencoding = "utf-8",
-	filetype = on,
-	guifont = "monospace:h17",
-	hlsearch = true,
-	ignorecase = true,
-	linebreak = true,
-	mouse = "a",
-	number = true,
-	numberwidth = 4,
-	pumheight = 10,
-	relativenumber = true,
-	scrolloff = 10,
-	shiftwidth = 2,
-	showmode = false,
-	sidescrolloff = 10,
-	signcolumn = "yes",
-	smartcase = true,
-	smartindent = true,
-	spelllang = "en_us",
-	spell = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	syntax = on,
-	tabstop = 2,
-	termguicolors = true,
-	timeoutlen = 300,
-	undofile = true,
-	updatetime = 300,
-	whichwrap = "bs<>[]hl",
-	wrap = true,
-	writebackup = false,
+  backup = false,
+  cmdheight = 1,
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 1,
+  cursorline = true,
+  expandtab = true,
+  fileencoding = "utf-8",
+  filetype = on,
+  guifont = "monospace:h17",
+  hlsearch = true,
+  ignorecase = true,
+  linebreak = true,
+  mouse = "a",
+  number = true,
+  numberwidth = 4,
+  pumheight = 10,
+  relativenumber = true,
+  scrolloff = 10,
+  shiftwidth = 2,
+  showmode = false,
+  sidescrolloff = 10,
+  signcolumn = "yes",
+  smartcase = true,
+  smartindent = true,
+  spelllang = "en_us",
+  spell = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  syntax = on,
+  tabstop = 2,
+  termguicolors = true,
+  timeoutlen = 300,
+  undofile = true,
+  updatetime = 300,
+  whichwrap = "bs<>[]hl",
+  wrap = true,
+  writebackup = false,
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 vim.opt.shortmess:append("c")
@@ -48,8 +48,8 @@ vim.opt.iskeyword:append("-")
 vim.opt.formatoptions:remove({ "c", "r", "o" })
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")
 
-vim.cmd("colorscheme habamax")
+vim.cmd("colorscheme zellner")
 
 if not vim.fn.exists("guin running") then
-	vim.o.t_Co = 256
+  vim.o.t_Co = 256
 end
